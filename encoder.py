896f3a453447df5abc36ed04037dc0720750fee1
wildanjr19 <wildansekolah11@gmail.com>
2025-08-23 00:59:33 +0700

    add: EncoderBlock

--- encoder.py
+++ encoder.py
@@ -1,2 +1,25 @@
 import torch
 import torch.nn as nn
+from modules import MultiHeadAttention, FeedForwardBlock, ResidualConnection
+
+class EncoderBlock(nn.Module):
+    """Encoder Block"""
+    def __init__(self, features: int, self_attention_block: MultiHeadAttention, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        """
+        Args:
+            self_attention_block : Multi head attention layer
+            feed_forward_block : FFN layer
+        """
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        # Add & Norm (2x)
+        self.add_norm = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        # fase 1
+        x = self.add_norm[0](x, lambda x: self.self_attention_block(x,x, x, src_mask))
+        # fase 2
+        x = self.add_norm[1](x, lambda x: self.feed_forward_block(x))
+
+        return x
