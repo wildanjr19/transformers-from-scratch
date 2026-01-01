a7fc66b2a458df0a241b431bc7eddd229b182bf8
wildanjr19 <wildansekolah11@gmail.com>
2026-01-01 09:56:08 +0700

    add: train.py

--- train.py
+++ train.py
@@ -1,4 +1,30 @@
 import torch
 import torch.nn as nn
+
+import torchtext.datasets as datasets
+from torch.optim.lr_scheduler import LambdaLR
+
+import torchmetrics
+from torch.utils.tensorboard import SummaryWriter
+
+# our scripts
+from transformers_model import build_transformers
+from datasets import BilingualDataset, causal_mask
+from config import get_config, get_weights_file
+
+# HF utils
 from datasets import load_dataset
 from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+# build model function
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformers(
+        vocab_src_len,
+        vocab_tgt_len, 
+        config['seq_len'],
+        config['seq_len'],
+        d_model=config['d_model']
+    )
