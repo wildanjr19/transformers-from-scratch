9af199beabe091e9540d9a8cb50819301a338c79
wildanjr19 <wildansekolah11@gmail.com>
2026-01-01 10:20:32 +0700

    add: train function

--- train.py
+++ train.py
@@ -7,10 +7,13 @@ from torch.optim.lr_scheduler import LambdaLR
 import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 
+from pathlib import Path
+
 # our scripts
 from transformers_model import build_transformers
 from datasets import BilingualDataset, causal_mask
 from config import get_config, get_weights_file
+from build_data import get_dataset
 
 # HF utils
 from datasets import load_dataset
@@ -28,3 +31,25 @@ def get_model(config, vocab_src_len, vocab_tgt_len):
         config['seq_len'],
         d_model=config['d_model']
     )
+    return model
+
+# training function
+def train_model(config):
+    # get device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    # buat folder untuk model
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    # get dataset
+    train_loader, val_loader, tokenizer_src, tokenizer_tgt = get_dataset(config)
+
+    # build model
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    # tensorboard writer -> untuk logging proses
+    writer = SummaryWriter(config['experiment_name'])
+
+    # optimizers
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
