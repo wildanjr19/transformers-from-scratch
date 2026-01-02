5fc083a3417749b24692bb8c3b21a6a58fd94423
wildanjr19 <wildansekolah11@gmail.com>
2026-01-02 09:35:49 +0700

    add: training loop

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 
 from pathlib import Path
+from tqdm import tqdm
 
 # our scripts
 from transformers_model import build_transformers
@@ -53,3 +54,37 @@ def train_model(config):
 
     # optimizers
     optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+    
+    # preload weights/training jika ada atau checkpoint
+    if config['preload']:
+        model_filename = get_weights_file(config, config['preload'])
+        print(f"Preload model from {model_filename}")
+        # load state
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        # load optimizer state
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+
+    # loss function
+    # PAD di ignore supaya tidak dihitung dalam loss
+    # label smoothing untuk regularisasi
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id("[PAD]"), label_smoothing=0.1)
+
+    # main train loop
+    for epoch in range(initial_epoch, config['num_epochs']):
+        # free memory first
+        torch.cuda.empty_cache()
+        model.train()
+        # buat tqdm untuk pantau proses di tiap batch
+        train_batch_iterator = tqdm(train_loader, desc=f"Epoch {epoch:03d}")
+        # loop tiap batch
+        for batch in train_batch_iterator:
+            # ambil/siapkan data input dari batch
+            encoder_input = batch['encoder_input'].to(device) # (b, seq_len)
+            decoder_input = batch['decoder_input'].to(device) # (b, seq_len)
+            encoder_mask = batch['encoder_mask'].to(device) # (b, 1, 1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device) # (b, 1, seq_len, seq_len)
